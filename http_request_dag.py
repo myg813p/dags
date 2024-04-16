bb8464a70ca83b727102b005ba4b184d859dcb77
gijungcpy <119914592+myg813p@users.noreply.github.com>
2024-04-16 15:26:19 +0900

    Update http_request_dag.py

--- http_request_dag.py
+++ http_request_dag.py
@@ -26,7 +26,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'start_date': datetime.now(),  # Set start_date to the current time for immediate execution
+    'start_date': datetime(2024, 4, 13, 0, 0),
     'timezone': 'Asia/Seoul',  # Set timezone to Asia/Seoul
 }
 
